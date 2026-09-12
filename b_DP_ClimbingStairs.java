9c6ae76bdc724ccb133d346bb561a22f8deea01b
Nilesh Prakash Barge <x25168304@student.ncirl.ie>
2026-09-12 13:01:35 +0530

    116. Climing Stairs
    # 116. Climing Stairs
    # Find number ways to reach top by taking one or two steps at a time
    # Bottom up dynamic programming

---
+++ b_DP_ClimbingStairs.java
@@ -0,0 +1,39 @@
+package dsa1;
+
+public class b_DP_ClimbingStairs {
+    public static void main(String[] args) {
+        int numOfStairsBottomup = numberOfStepsBottomup(5);
+        System.out.println("DP bottom up approach, Number of stairs: " + numOfStairsBottomup);
+
+        int numOfStairsFib = numberOfStepsFibonacci(5);
+        System.out.println("Fibonacci approach, Number of stairs: " + numOfStairsFib);
+
+    }
+
+    public static int numberOfStepsFibonacci(int num) {
+        int first = 1, second = 2, result = 0;
+        if(num<=2) {
+            return num;
+        }
+
+        for(int i=3; i<=num; i++) {
+            result = first+second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+
+    public static int numberOfStepsBottomup(int num) {
+        int[] arr = new int[num+1];
+        if(num <=2) {
+            return num;
+        }
+        //base case
+        arr[0]=0; arr[1]=1; arr[2]=2;
+        for(int i=3; i<=num;i++) {
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr[num];
+    }
+}
