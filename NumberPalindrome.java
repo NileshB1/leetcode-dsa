29410cde8a3a5397d7b29918e40181eb29f7266b
Nilesh Prakash Barge <x25168304@student.ncirl.ie>
2026-09-21 23:42:01 +0530

    NumberPalindrome: Check if a number is Palindrome
    Given an integer n, determine whether it is a palindrome number or not. A number is called a palindrome if it reads the same from forward and backward.
    
    Examples:
    
    Input: n = 12321
    Output: True
    Explanation: 12321 is a palindrome number because it reads same  forward and backward.
    
    Input: n = -121
    Output: True
    Explanation:  We number is palindrome, we mainly ignore sign.
    
    Input: n = 1234
    Output:  False
    Explanation: 1234 is not a palindrome number because it does not read the same forward and backward.

---
+++ NumberPalindrome.java
@@ -0,0 +1,28 @@
+package dsa1;
+
+public class NumberPalindrome {
+    public static void main(String[] args) {
+        int num = 12321;
+        // int num = -121;
+        // int num = 1234;
+        boolean isNumPalidrome = isPalindrome(num);
+        System.out.println("Number: "+ num + ", is palindrome: " + isNumPalidrome);
+    }
+
+    public static boolean isPalindrome(int num) {
+        if(num <=0) {
+            return false;
+        }
+        if(num>0 && num%10==0) {
+            return false;
+        }
+        int revesedNum = 0;
+        int lastDigit = 0;
+        while(num > revesedNum) {
+            lastDigit = num%10;
+            revesedNum = (revesedNum*10) + lastDigit;
+            num = num/10;
+        }
+        return (num == revesedNum) || (num == revesedNum/10);
+    }
+}
